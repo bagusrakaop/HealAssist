f75364176038e52baa12fcb6dece1f931076b5e4
giftormdh <giftoramadha@gmail.com>
2024-03-31 01:51:23 +0700

    add: set font

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({
+  weight: ["300", "400", "600", "700", "800"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
