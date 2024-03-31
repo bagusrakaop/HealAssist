90ea9a7b6c3961be37f148efa737c95c48bb404b
giftormdh <giftoramadha@gmail.com>
2024-04-01 01:24:50 +0700

    feat: create landing page (2/7 segment)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,111 +2,61 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-full bg-white min-h-screen">
+      {/* top of landing page */}
+      <div className="flex h-[480px] bg-neutral text-primary items-center justify-center">
+        <div className="w-1/3">
+          <div className="text-xs">For a Better Quality of Life</div>
+          <div className="flex text-5xl">
+            <p className="font-bold">Heal</p>Assist
+          </div>
+          <div className="text-3xl">Present for you</div>
+          <div>with the power of AI, we helps you live a healthy routine and prevents you from cardiovascular disease</div>
+          <button className="btn btn-primary mt-5">Our Features</button>
         </div>
+        <Image className="items-center right-0" src="/robot-1.svg" alt="Diagbostic Robot" width={480} height={480} priority />
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      {/* Cardiovascular Disease Case Information */}
+      <div className="flex flex-col w-2/3 mx-auto h-[640px] bg-white text-primary justify-center items-center">
+        <div className="text-3xl font-extrabold mb-10">Did you Know??</div>
+        <div className="flex py-5">
+          <div className="flex mx-10">
+            <Image className="mr-2 -translate-y-2" src="/illustration-stroke.svg" alt="Stroke Illustration" width={72} height={72} priority />
+            <div>
+              <p className="font-bold text-2xl">331,349</p>
+              <p className="text-xs">
+                Deaths Caused <br /> by Stroke
+              </p>
+            </div>
+          </div>
+          <div className="flex mx-10">
+            <Image className="mr-2" src="/illustration-coronary-heart.svg" alt="Coronary Heart Illustration" width={64} height={64} priority />
+            <div>
+              <p className="font-bold text-2xl">245,343</p>
+              <p className="text-xs">
+                Deaths Caused <br /> by Coronary Heart
+              </p>
+            </div>
+          </div>
+          <div className="flex mx-10">
+            <Image className="mr-2" src="/illustration-hypertensive-heart.svg" alt="Hypertensive Heart Illustration" width={64} height={64} priority />
+            <div>
+              <p className="font-bold text-2xl">50,620</p>
+              <p className="text-xs">
+                Deaths Caused <br /> by Hypertensive Heart
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="text-center my-10">
+          <p className="font-bold">
+            Currently, heart and blood vessel disease or cardiovascular disease is still the highest cause of death in the world. WHO states that more than 17 million people in the world die from
+            heart and blood vessel disease. Meanwhile, deaths in Indonesia due to cardiovascular disease reached 651,481 people per year, consisting of stroke 331,349 deaths, coronary heart disease
+            245,343 deaths, hypertensive heart disease 50,620 deaths, and other cardiovascular diseases (Institute for Health Matrics and Evaluation, 2019).{" "}
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <br />
+          <p className="text-xs">Quoted from sehatnegeriku.kemkes.go.id</p>
+        </div>
       </div>
     </main>
   );
