185869db512e631882ee1e9441e997fa57b5cd7c
imelnath <fiomelnathany@gmail.com>
2024-05-27 08:28:08 +0700

    fix: show user favorites

--- frontend/src/services/favorites.services.js
+++ frontend/src/services/favorites.services.js
@@ -4,37 +4,75 @@ import Cookies from "js-cookie";
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const getAllFoods = async () => {
-  let token = Cookies.get("token");
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`${BASE_URL}/foods`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        resolve(response?.data);
-      })
-      .catch((err) => {
-        reject(err.response?.data);
-      });
-  });
+    let token = Cookies.get("token");
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${BASE_URL}/foods`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                resolve(response?.data);
+            })
+            .catch((err) => {
+                reject(err.response?.data);
+            });
+    });
 };
 
 export const getAllExercises = async () => {
-  let token = Cookies.get("token");
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`${BASE_URL}/exercises`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        resolve(response?.data);
-      })
-      .catch((err) => {
-        reject(err.response?.data);
-      });
-  });
+    let token = Cookies.get("token");
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${BASE_URL}/exercises`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                resolve(response?.data);
+            })
+            .catch((err) => {
+                reject(err.response?.data);
+            });
+    });
+};
+
+export const getUserExercises = async () => {
+    let token = Cookies.get("token");
+    let id = Cookies.get("id");
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${BASE_URL}/exercises/user/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                resolve(response?.data);
+            })
+            .catch((err) => {
+                reject(err.response?.data);
+            });
+    });
+};
+
+export const getUserFoods = async () => {
+    let token = Cookies.get("token");
+    let id = Cookies.get("id");
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${BASE_URL}/foods/user/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                resolve(response?.data);
+            })
+            .catch((err) => {
+                reject(err.response?.data);
+            });
+    });
 };
